83f825b7473ad362cb5ae5cd0989af7a54f48b71
alex garcia <alex.g9415@gmail.com>
2021-12-02 15:40:29 -0800

    add styling

--- components/HomeLatestPosts/HomeLatestPosts.js
+++ components/HomeLatestPosts/HomeLatestPosts.js
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from "react"
 import PostPreview from "../PostPreview/PostPreview";
 
-export default function HomeLatestPosts ({posts}) {
+import Row from "react-bootstrap/Row"
+
+export default function HomeLatestPosts({ posts }) {
     const [latestPosts, setLatestPosts] = useState([]);
 
     useEffect(() => {
-        setLatestPosts(posts.slice(0,5));
+        setLatestPosts(posts.slice(0, 5));
     }, [posts])
 
     const previews = latestPosts.map((post) => <PostPreview post={post} key={post.id} />);
 
     return (
-        <>
-            <h2>Latest Posts</h2>
-            {previews}
-        </>
+        <section className="featuredPosts">
+            <Row xs={1} md={2} className="g-4">
+                {previews}
+            </Row>
+        </section>
     )
 }
