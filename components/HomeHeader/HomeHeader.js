00fc67c72d9280b192d951917586ca43518d7793
alex garcia <alex.g9415@gmail.com>
2021-12-02 15:39:24 -0800

    CSS modifications. Add moving text.

--- components/HomeHeader/HomeHeader.js
+++ components/HomeHeader/HomeHeader.js
@@ -6,12 +6,18 @@ import Col from "react-bootstrap/Col"
 
 function HomeHeader(props) {
     return (
-        <section className="text-center">
+        <section className="text-center homeHeader">
             <Container>
                 <Row className="py-large-5">
-                    <Col className="col-lg-6 col-md-8 mx-auto">
+                    <Col className="col-lg-6 col-md-8 mx-auto Iam">
                         <h1 className="fw-light">The Socal Azure Admin</h1>
-                        <p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum fugit aperiam explicabo consectetur! Accusamus qui perspiciatis delectus veritatis reiciendis illo fugit eaque asperiores, adipisci cum iste corrupti consequuntur, itaque laborum?</p>
+                        <b>
+                            <div className="lead innerIam">
+                                Azure<br />
+                                Intune<br />
+                                Web Development
+                            </div>
+                        </b>
                     </Col>
                 </Row>
             </Container>
