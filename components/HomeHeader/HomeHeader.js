275c56d6e2e117ae6f11b76e19f9d870c66488a5
Alex Garcia <alex.g9415@gmail.com>
2021-12-01 20:42:30 -0800

    implement react-boostrap, implement projects page, css fixes

--- components/HomeHeader/HomeHeader.js
+++ components/HomeHeader/HomeHeader.js
@@ -1,16 +1,21 @@
 import React from "react";
-import axios from "axios";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 function HomeHeader(props) {
-    console.log(props.homeData)
     return (
-        <div className="jumbotron">
-            <section className="container">
-                <h1>{props.homeData.title}</h1>
-                <h2>{props.homeData.header}</h2>
-                <p>{props.homeData.description}</p>
-            </section>
-        </div>
+        <section className="text-center">
+            <Container>
+                <Row className="py-large-5">
+                    <Col className="col-lg-6 col-md-8 mx-auto">
+                        <h1 className="fw-light">The Socal Azure Admin</h1>
+                        <p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum fugit aperiam explicabo consectetur! Accusamus qui perspiciatis delectus veritatis reiciendis illo fugit eaque asperiores, adipisci cum iste corrupti consequuntur, itaque laborum?</p>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 }
 
--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,27 +1,47 @@
 import React from "react";
 import Link from "next/link";
 
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav"
+
 function NavBar() {
     return (
-        <nav className="NavBar">
-            <div className="container">
-                <Link href="/">
-                    <a className="title">The Socal Azure Admin</a>
-                </Link>
-                <ul>
-                    <li>
-                        <Link href="/about">
-                            <a>About Me</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/posts">
-                            <a>Blog</a>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Brand>
+                    <Link href="/">
+                        The Socal Azure Admin
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Collapse>
+                    <Nav className="ms-auto">
+                        <Nav.Item>
+                            <Nav.Link>
+                                <Link href="/about">
+                                    About Me
+                                </Link>
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link>
+                                <Link href="/posts">
+                                    Blog
+                                </Link>
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link>
+                                <Link href="/projects">
+                                    Projects
+                                </Link>
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
