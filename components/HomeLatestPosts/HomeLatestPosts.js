895983ece1f293f0f1837f6c239d2bf061bbf259
Alex Garcia <alex.g9415@gmail.com>
2021-12-01 21:11:59 -0800

    integrate card component react bootstrap

--- components/HomeLatestPosts/HomeLatestPosts.js
+++ components/HomeLatestPosts/HomeLatestPosts.js
@@ -1,25 +1,19 @@
 import React, { useEffect, useState } from "react"
 import PostPreview from "../PostPreview/PostPreview";
 
-function HomeLatestPosts ({posts}) {
+export default function HomeLatestPosts ({posts}) {
     const [latestPosts, setLatestPosts] = useState([]);
 
     useEffect(() => {
         setLatestPosts(posts.slice(0,5));
     }, [posts])
 
-    function renderPostPreviews() {
-        return latestPosts.map((post) => {
-            return <PostPreview post={post} key={post.id}/>
-        })
-    }
+    const previews = latestPosts.map((post) => <PostPreview post={post} key={post.id} />);
 
     return (
         <>
             <h2>Latest Posts</h2>
-            {renderPostPreviews()}
+            {previews}
         </>
     )
 }
-
-export default HomeLatestPosts
