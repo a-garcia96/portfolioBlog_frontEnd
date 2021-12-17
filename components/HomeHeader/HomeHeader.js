60f2e374dfc382c0b88292a942bd76eeba3532e4
alex garcia <alex.g9415@gmail.com>
2021-12-17 14:40:02 -0800

    integrate strapi data

--- components/HomeHeader/HomeHeader.js
+++ components/HomeHeader/HomeHeader.js
@@ -4,19 +4,20 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
-function HomeHeader(props) {
+function HomeHeader({homeData}) {
+
     return (
         <section className="text-center homeHeader">
             <Container>
                 <Row className="py-large-5">
                     <Col className="col-lg-6 col-md-8 mx-auto Iam">
-                        <h1 className="fw-light">The Socal Azure Admin</h1>
+                        <h1 className="fw-light">{homeData.header}</h1>
                         <b>
                             <div className="lead innerIam">
-                                Azure<br />
-                                Intune<br />
-                                PowerShell<br />
-                                Web Development
+                                {homeData.veritcalText1}<br />
+                                {homeData.verticalText2}<br />
+                                {homeData.verticalText3}<br />
+                                {homeData.verticalText4}
                             </div>
                         </b>
                     </Col>
