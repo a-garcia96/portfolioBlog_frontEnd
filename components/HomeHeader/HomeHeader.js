9b9b80f44cc4487925e6eaa6e82728f38005964a
Alex Garcia <alex.g9415@gmail.com>
2021-12-23 20:56:06 -0800

    Implement home profile photo and styling

--- components/HomeHeader/HomeHeader.js
+++ components/HomeHeader/HomeHeader.js
@@ -1,14 +1,26 @@
 import React from "react";
+import Image from 'next/image'
+
+import profilePhoto from "../../public/images/profilePhoto.jpg"
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
-function HomeHeader({homeData}) {
+function HomeHeader({ homeData }) {
 
     return (
         <section className="text-center homeHeader">
             <Container>
+                <Row className="py-large-5 image-row">
+                    <Col className="col-lg-6 col-md-8 mx-auto">
+                        <div className="center-utility">
+                            <div className="profile-frame">
+                                <Image alt="Alex Garcia" height={250} width={250} quality={50} objectFit="cover" priority src={profilePhoto} />
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
                 <Row className="py-large-5">
                     <Col className="col-lg-6 col-md-8 mx-auto Iam">
                         <h1 className="fw-light">{homeData.header}</h1>
