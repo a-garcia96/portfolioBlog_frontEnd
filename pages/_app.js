16ce2545fbe9dd1d56046b7d041a25517b24220e
alex garcia <alex.g9415@gmail.com>
2022-01-26 07:36:04 -0800

    add meta tags in head

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import NavBar from "../components/NavBar/NavBar"
 import "../styles/index.scss";
+import Head from "next/head"
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+      </Head>
       <NavBar />
       <Component {...pageProps} />
     </>
