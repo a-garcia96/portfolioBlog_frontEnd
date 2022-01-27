6d3449f16c1f6b2bdfc031a4fc94ecf972e65039
alex garcia <alex.g9415@gmail.com>
2022-01-27 09:00:47 -0800

    utilize color variables

--- components/HomeHeader/HomeHeader.js
+++ components/HomeHeader/HomeHeader.js
@@ -13,10 +13,10 @@ function HomeHeader({ homeData }) {
                 <div className="header-text">
                     <h1>
                         <span className="header-text-greeting"><span className="header-text-highlight">👋🏽 Hello there,</span> my name is</span>
-                        <span className="header-text-name">Alejandro Garcia.</span>
-                        <span className="header-text-title">I am a <span className="header-text-highlight">Systems Administrator.</span></span>
+                        <span className="header-text-name">Alex Garcia.</span>
+                        <span className="header-text-title">I build <span className="header-text-highlight">infrastructure for the web.</span></span>
                     </h1>
-                    <p className="header-text-desc">I specialize in Intune, Azure and PowerShell. Currently I am focused on getting into the world of DevOps and learning about Terraform and Ansible.</p>
+                    <p className="header-text-desc">I specialize in Intune, Azure and PowerShell. Currently I am focused on getting into the world of DevOps and adopting Terraform and Ansible to build automated infrastructure deployment pipelines.</p>
                     <button className="btn-header"><Link href="/resume">See my experience</Link></button>
                 </div>
             </div>
--- pages/404.js
+++ pages/404.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function custom404() {
     return (
-        <div className="custom-404-container">
-            <div className="fourohfour-text-box">
-                <h1>Uh Oh!</h1>
-                <hr />
-                <p>This site is still being built. <br/> More coming soon!</p>
+            <div className="custom-404-container">
+                <div className="fourohfour-text-box">
+                    <h1>Uh Oh!</h1>
+                    <hr />
+                    <p>This site is still being built. <br /> More coming soon!</p>
+                </div>
             </div>
-        </div>
     );
 }
