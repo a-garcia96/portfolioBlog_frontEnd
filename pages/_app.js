1f07d170a203475d7047b3f6172952bcede030ac
alex garcia <alex.g9415@gmail.com>
2022-01-27 09:43:21 -0800

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
       </Head>
       <NavBar />
       <Component {...pageProps} />
