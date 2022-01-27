53a1aaf8a41c838a9cca398bb83e79be7ef6dbcd
alex garcia <alex.g9415@gmail.com>
2022-01-27 08:50:09 -0800

    add particles

--- components/HomeHeader/HomeHeader.js
+++ components/HomeHeader/HomeHeader.js
@@ -5,6 +5,10 @@ function HomeHeader({ homeData }) {
 
     return (
         <section className="home-header">
+            <div className="particle particle-1"></div>
+            <div className="particle particle-2"></div>
+            <div className="particle particle-3"></div>
+            <div className="particle particle-4"></div>
             <div className="container">
                 <div className="header-text">
                     <h1>
